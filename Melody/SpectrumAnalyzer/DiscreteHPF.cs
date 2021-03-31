1968ceb9000b651c26960e7008bba0ce5cd45363
AlanRo <alekroga@gmail.com>
2021-03-31 21:24:46 +0500

    Add frequency filters

---
+++ Melody/SpectrumAnalyzer/DiscreteHPF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.SpectrumAnalyzer
+{
+    class DiscreteHPF
+    {
+        public static void Filter(Complex[][] spectrum, double winDuration, double borderFreq)
+        {
+            var freqMult = 1 / winDuration;
+            var freqsCount = spectrum[0].Length / 2;
+            for (var i = freqsCount - 1; i >= 0 && i * freqMult > borderFreq; i++)
+            {
+                for (var j = 0; j < spectrum.Length; j++)
+                {
+                    spectrum[j][i] = Complex.Zero;
+                    spectrum[j][freqsCount * 2 - i - 1] = Complex.Zero;
+                }
+            }
+        }
+    }
+}
---
+++ Melody/SpectrumAnalyzer/DiscreteLPF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.SpectrumAnalyzer
+{
+    public static class DiscreteLPF
+    {
+        public static void Filter(Complex[][] spectrum, double winDuration, double borderFreq)
+        {
+            var freqMult = 1 / winDuration;
+            var freqsCount = spectrum[0].Length / 2;
+            for (var i = 0; i < freqsCount && i * freqMult < borderFreq; i++)
+            {
+                for (var j = 0; j < spectrum.Length; j++)
+                {
+                    spectrum[j][i] = Complex.Zero;
+                    spectrum[j][freqsCount * 2 - i - 1] = Complex.Zero;
+                }
+            }
+        }
+    }
+}
